00f52f3f9bb0150138ca64038d1fc34704339ce4
Filipe Soares <filipecarlossoares@gmail.com>
2020-11-29 21:17:33 -0300

    first commit

---
+++ src/main/java/com/filipe/EAgendaApiApplication.java
@@ -0,0 +1,13 @@
+package com.filipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAgendaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EAgendaApiApplication.class, args);
+	}
+
+}
