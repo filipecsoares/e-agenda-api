737722d8677af4a2f5095c53591b349f6ba45a58
Filipe Soares <filipecarlossoares@gmail.com>
2020-12-15 19:23:02 -0300

    create userController

--- src/main/java/com/filipe/agenda/controller/UserController.java
+++ src/main/java/com/filipe/agenda/controller/UserController.java
@@ -1,14 +1,17 @@
 package com.filipe.agenda.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
-	
-	@GetMapping("/users")
-	public String getAll() {
-		return "Hello World";
+
+	@GetMapping
+	public ResponseEntity<?> getAll() {
+		return ResponseEntity.ok("It's Ok");
 	}
 
 }
